ac9cf5d64c38920ffba218e4b0f3b6357da16618
mostafa jamali <m.jamali20274@gmail.com>
2020-08-17 20:26:50 +0430

    Updates

---
+++ precache-manifest.9b271997281dff6c6a152ada1334e67a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa57dab3983ac050da5598602d8c8e7",
+    "url": "/Todo-by-Redux/index.html"
+  },
+  {
+    "revision": "f93d25fcbc938fd5eefd",
+    "url": "/Todo-by-Redux/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "10dc5a00dd0ac49a4552",
+    "url": "/Todo-by-Redux/static/css/main.b6344ee0.chunk.css"
+  },
+  {
+    "revision": "f93d25fcbc938fd5eefd",
+    "url": "/Todo-by-Redux/static/js/2.274e698a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Todo-by-Redux/static/js/2.274e698a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10dc5a00dd0ac49a4552",
+    "url": "/Todo-by-Redux/static/js/main.d86cf15c.chunk.js"
+  },
+  {
+    "revision": "d7033509013024f884a1",
+    "url": "/Todo-by-Redux/static/js/runtime-main.d61ee614.js"
+  }
+]);
