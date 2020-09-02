21cfc039f34e57f05296f918051ed904f0be9052
mostafa jamali <m.jamali20274@gmail.com>
2020-09-02 13:37:37 +0430

    add redux-persist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './Redux/store';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -12,20 +13,22 @@ import TodoArchive from './view/TodoArchive/TodoArchive';
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Router>
-          <div>
-            <Switch>
-              <Route path="/edit/:todoId">
-                <TodoArchive />
-              </Route>
-              <Route path="/">
-                <TodoArchive />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="App">
+          <Router>
+            <div>
+              <Switch>
+                <Route path="/edit/:todoId">
+                  <TodoArchive />
+                </Route>
+                <Route path="/">
+                  <TodoArchive />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,23 @@
 import { applyMiddleware, createStore } from 'redux';
-import RootReducer from './RootReducer';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger';
 
-const middlewares = [logger];
+import RootReducer from './RootReducer';
 
-export const store = createStore(
-    RootReducer,
+const middlewares = [logger];
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ["todoList"]
+}
+
+const persistedReducer = persistReducer(persistConfig, RootReducer)
+
+const store = createStore(
+    persistedReducer,
     applyMiddleware(...middlewares)
 )
+export default store;
+
+export const persistor = persistStore(store)
